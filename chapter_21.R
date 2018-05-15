b927113f75636a447f85a45e2cf3bd73b9fc33c2
Edward Ross <edward@skeptric.com>
2018-05-15 14:33:43 +1000

    Haar Wavelets

--- chapter_21.R
+++ chapter_21.R
@@ -16,10 +16,6 @@ glass <- read.table('glass.dat')
 # sigma%2 = n/k sum(i=n-k+1, n) beta_j^2, k = n/4
 # Bands: c = RMS(phi_j(x)) * sigma * chi_J, a / sqrt(n)
 
-# Haar
-# phi_j, k (x) = 2^(j/2) phi(2^j * x - k)
-# phi(x) = {-1 0 <= x <= 1/2, 1 1/2 < x <= 1)
-
 
 # X = RI, Y = Al
 # Exercise 7
@@ -93,7 +89,8 @@ a2 <- function(x) {
 }
 
 cbound <- function (x) {
-    qchisq((1-0.05 / 2), jhat) * sqrt(a2(x) * sigmahat2 / length(al))
+    # 95% confidence interval
+    qchisq(1 - 0.05, jhat) * sqrt(a2(x) * sigmahat2 / length(al))
 }
 
 library(dplyr)
@@ -108,3 +105,106 @@ glass %>% mutate(pred = reg_est(Al),
     geom_point() +
     geom_rug() +
     geom_line(aes(y=pred))
+
+
+
+# Haar wavelets:
+
+# I'm getting numbers 1000 times too big!
+
+# phi_j, k (x) = 2^(j/2) phi(2^j * x - k)
+# phi(x) = {-1 0 <= x <= 1/2, 1 1/2 < x <= 1)
+
+# Father wavelet
+haarf <- function (x) { (x >= 0) * (x <= 1) }
+# Mother wavelet
+haarm <- function(x) { (x >= 0) * (x  <=1) * ifelse(x <= 0.5, -1, 1) }
+# This is the general wavelet formula; with haarm replaced by mother wavelet
+haar <- function(j, k, x) { 2^((j-1)/2) * haarm(2^(j-1) * x - (k-1)) }
+
+
+haar_regression <- function(x, y) {
+    n <- length(x)
+    if (n != length(y)) {
+        stop("x and y must be same length")
+    }
+
+    # Estimate the coefficients
+    # Obviously haarf(al) == 1, but shows structure for different wavelet
+    alpha <- mean(y * haarf(x))
+
+    D <- list()
+    for (i in 1:floor(log(n, 2))) {
+        D[[i]] <- double(2^(i-1))
+        for (j in seq_along(D[[i]])) {
+            D[[i]][j] <- mean(y * haar(i, j, x))
+        }
+    }
+
+    # It's not clear to me that this estimate is any good when the distribution is
+    # far from normal (and if it's normal why are you using wavelets?)
+
+    # Unfortunately this thresholds everything to zero!
+    sigmahat <- sqrt(n) * median(abs(D[[i]])) / 0.6745
+
+    #sigmahat <- 0
+
+    # Use universal thresholding; this is similar to a BIC approach
+    # Trying to create the smallest model instead of most accurate
+    haar_beta <- list()
+    for (i in 1:floor(log(n, 2))) {
+        haar_beta[[i]] <- double(2^(i-1))
+        for (j in seq_along(haar_beta[[i]])) {
+            if (abs(D[[i]][j]) > sigmahat * sqrt(2*log(n) / n)) {
+                haar_beta[[i]][j] <- D[[i]][j]
+            }
+        }
+    }
+
+    list(beta=haar_beta, alpha=alpha)
+}
+
+wavelet_reg <- function(haar_coeff, x) {
+    ans <- haar_coeff$alpha * haarf(x)
+    for (i in seq_along(haar_coeff$beta)) {
+        for (j in seq_along(haar_coeff$beta[[i]])) {
+            coeff <- haar_coeff$beta[[i]][j]
+            ans <- ans + coeff * haar(i, j, x)
+        }
+    }
+    ans
+}
+
+glass_haar <- haar_regression(al, ri)
+
+# Doesn't look very good...
+t <- seq(almin, almin + alrange, 0.01)
+plot(glass$Al, glass$RI)
+lines(t, wavelet_reg(glass_haar, (t - almin) / alrange ))
+rug(glass$Al)
+rug(glass$RI, side=2)
+
+
+
+# Exercise 9 b
+
+doppler <- function(x) { sqrt(x*(1-x)) * sin ( 2.1 * pi / (x + 0.05)) }
+
+t <- seq(1/1024, 1, 1/1024)
+y <- doppler(t) + 0.1 * rnorm(length(t))
+
+
+doppler_haar <- haar_regression(t, y)
+
+
+plot(t, y)
+lines(t, wavelet_reg(doppler_haar, t))
+
+
+# Exercise 11: Start
+n <- 100
+nsamples <- 1000
+x <- matrix(rnorm(n * nsamples), nrow = n)
+
+samples <- apply(x, 2, function(x) median(abs(x)))
+mean(samples)
